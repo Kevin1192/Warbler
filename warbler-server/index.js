25c423e53b92a1b4d535afed60bf15af9ce129f8
Kevin <691208yz@gmail.com>
2020-06-01 15:18:33 -0400

    add auth routes

--- warbler-server/index.js
+++ warbler-server/index.js
@@ -1,13 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = 8081;
 const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth')
+
+const errorHandler = require('./handlers/error')
 
 app.use(cors());
 
 app.use(bodyParser.json());
 
+app.use('/api/auth', authRoutes)
 // all my routes here 
 
 app.use(function(req, res, next){
@@ -15,6 +20,8 @@ app.use(function(req, res, next){
     err.status = 404
     next(err)
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`)
