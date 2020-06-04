9d4faa71bcc3786ed75ae324ccd9b8ee1e69b0c8
Kevin <691208yz@gmail.com>
2020-06-03 20:28:22 -0400

    back-end done

--- warbler-server/index.js
+++ warbler-server/index.js
@@ -4,8 +4,11 @@ const app = express();
 const cors = require('cors');
 const PORT = 8081;
 const bodyParser = require('body-parser');
-const authRoutes = require('./routes/auth')
+const authRoutes = require('./routes/auth');
+const messagesRoutes = require('./routes/messages');
+const { loginRequired, ensureCorrectUser } = require('./middleware/auth')
 
+const db = require('./models/index')
 const errorHandler = require('./handlers/error')
 
 app.use(cors());
@@ -13,7 +16,23 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes)
+app.use('/api/users/:id/messages', loginRequired, ensureCorrectUser, messagesRoutes)
 // all my routes here 
+
+app.get('/api/messages', loginRequired, async function(req, res, next){
+    try {
+        let messages = await db.Message.find()
+            .sort({createdAt: 'desc'})
+            .populate('user', {
+                username: true,
+                profileImageUrl: true
+            })
+            return res.status(200).json(messages);
+
+    } catch (err) {
+        return next(err)
+    }
+})
 
 app.use(function(req, res, next){
     let err = new Error('Not Found')
--- warbler-server/models/index.js
+++ warbler-server/models/index.js
@@ -5,7 +5,9 @@ mongoose.Promise = Promise;
 
 mongoose.connect('mongodb://localhost/warbler', {
     keepAlive: true,
-    useMongoClient: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 module.exports.User = require('./user')
+module.exports.Message = require('./message')
